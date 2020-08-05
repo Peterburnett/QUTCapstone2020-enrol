c225ea9e3842e4e2b4a7dff211f1a3f2be36bd58
qnguy29 <quyen_jaeger@outlook.com>
2020-08-05 17:18:22 +1000

    Adding plugin dependency

--- version.php
+++ version.php
@@ -5,3 +5,9 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2019111802;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2019111200;        // Requires this Moodle version
 $plugin->component = 'enrol_payment';      // Full name of the plugin (used for diagnostics)
+
+// Adding plugin dependencies 
+$plugin->dependencies = array(
+    'mod_foo' => ANY_VERSION,   // The Foo activity must be present (any version).
+    'enrol_plugin' => 2019111200, // The enrolment plugin version 2019111200 or higher must be present.
+);
