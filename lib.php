882ef1f568cdfe228933eb9f1dfb7a1c65664444
Erunx <50323955+Erunx@users.noreply.github.com>
2020-08-26 16:21:45 +1000

    Fixed changes

--- lib.php
+++ lib.php
@@ -1,32 +1,39 @@
-
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * Payment enrolment plugin.
+ *
+ * This plugin allows you to set up paid courses.
+ *
+ * @package    enrol_payment
+ * @copyright  2020 Aaron Dang
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 defined('MOODLE_INTERNAL') || die();
 class enrol_payment_plugin extends enrol_plugin {
 
      public function enrol_page_hook(stdClass $instance){  
-         global $CFG, $USER, $OUTPUT, $PAGE, $DB;
+         global $CFG, $USER, $OUTPUT, $PAGE, $DB, $html;
+        $html .= html_writer::tag('p',get_string('enrol','enrol_payment'));
+        $html .= html_writer::tag('p',get_string('price','enrol_payment'));
+        $html .= html_writer::tag('button onClick="window.location=\'' . $CFG->wwwroot . '\';"','enrol now');
 
-        ob_start();
-        
-        echo "<p><b>To enrol into a class please pay the amount below</b></p>";
-
-        echo "<p>Price: $</p>";
-
-        if(array_key_exists('paymentButton',$_POST)){
-            echo "<p>button selected</p>";
-        }
-        
-        echo  '<button onClick="window.location=\'' . $CFG->wwwroot . '\';">Enrol Now!</button>';
-        
-        
-
-        return $OUTPUT->box(ob_get_clean());
+        return $html;
      }
-    
-  //  public function get_course_price(courseid){
-//
-  //  }
     
     
 
