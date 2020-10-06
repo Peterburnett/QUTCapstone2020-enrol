8a547b71cfd6247ca2d8be3fd8eab6fdc8b71b15
harunaka <51142777+harunaka11037@users.noreply.github.com>
2020-10-06 13:38:13 +1000

    Lower required version

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2020082600;
-$plugin->requires  = 2019111200;
+$plugin->requires  = 2018051714;
 $plugin->component = 'enrol_payment';
 
 // Adding plugin dependencies to paymentplugin.
